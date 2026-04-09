3e76ab7c32df1e7df96318cc79adbd258d4bb09e
suhyunnnnnni <dastar998176@gmail.com>
2026-04-09 17:33:51 +0900

    함수화 0대신 공백으로 변경

--- timesTables.js
+++ timesTables.js
@@ -1,24 +1,26 @@
-// 단수 
-let table = 1;
-
-while( table < 13 ) {
-
-    console.log(`${table}단`)
-
-    for(let i = 1; i < 10; i++){
-
-        if(table < 10){
-            console.log(`0${table} X 0${i} = ${table*i}`)
-        }else{
-            console.log(`${table} X 0${i} = ${table*i}`)
+// tableStart → 시작 단수, tableEnd → 끝 단수
+function timesTable( tableStart, tableEnd ){
+    
+    for( let table = tableStart; table <= tableEnd; table++ ) {
+    
+        console.log(`${table}단`)
+    
+        for(let i = 1; i < 10; i++){
+    
+            // 마지막 단수 길이에 맞춰 공백 생성
+            const t = String(table).padStart(String(tableEnd).length, ' ');
+    
+            console.log(`${t} X ${i} = ${table*i}`)
+    
         }
+    
+        // 단과 단사이의 공백
+        console.log(`\n`)
+    };
 
-    }
-    // 단과 단사이의 공백
-    console.log(`\n`)
+}
 
-    table = table + 1;
-};
 
+timesTable(99, 100)
 
 
